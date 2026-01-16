9a410b558fc23578358e984786d079a3bb31465b
Grupo Via <grupoviabcn@gmail.com>
2026-01-16 11:18:06 +0100

    prueba de bienvenida

---
+++ frontend/lib/pages/welcome_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bienvenido'),
+      ),
+      body: const Center(
+        child: Text(
+          'Bienvenido a AppGV',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
