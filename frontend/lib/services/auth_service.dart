b225d28377fd868234b22fa5976213f19552b539
Grupo Via <grupoviabcn@gmail.com>
2026-02-02 11:58:57 +0100

    añadir comentarios

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  final String _baseUrl = 'http://10.0.2.2:8000/api'; // Use 10.0.2.2 for Android emulator
+  final String _baseUrl = 'http://127.0.0.1:8000/api'; // Use 10.0.2.2 for Android emulator
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     final response = await http.post(
